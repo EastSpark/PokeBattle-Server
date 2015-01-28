220f50174b8365772e30e24dd617f6acaa35dab1
Charlie Phillips <charliephillips007@gmail.com>
2015-01-28 10:28:35 -0800

    Delete Averagemons

--- pbserver/mods/averagemons/scripts.js
+++ pbserver/mods/averagemons/scripts.js
@@ -1,7 +1,1 @@
-exports.BattleScripts = {
-	init: function () {
-		for (var i in this.data.Pokedex) {
-			this.modData('Pokedex', i).baseStats = {hp:100, atk:100, def:100, spa:100, spd:100, spe:100};
-		}
-	}
-};
+
